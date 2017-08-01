b83916e3cdbe6c8ebc20fd1971731aae89d0c653
Ari <ariel.sosnovsky@gmail.com>
2017-08-01 11:27:24 -0400

    no more dups

--- Draggable.d.ts
+++ Draggable.d.ts
@@ -58,66 +58,6 @@ declare namespace gsap {
 
 }
 
-declare namespace gsap {
-
-  export class Draggable {
-
-    static version: string;
-    static zIndex: number;
-
-    allowEventDefault: boolean;
-    autoScroll: number;
-    endRotation: number;
-    endX: number;
-    endY: number;
-    isDragging: boolean;
-    isPressed: boolean;
-    lockAxis: boolean;
-    lockedAxis: string;
-    maxRotation: number;
-    maxX: number;
-    maxY: number;
-    minRotation: number;
-    minX: number;
-    minY: number;
-    pointerEvent: PointerEvent;
-    pointerX: number;
-    pointerY: number;
-    rotation: number;
-    tween: TweenLite;
-    vars: Draggable.Vars;
-    x: number;
-    y: number;
-
-    constructor(target: Object, vars?: Draggable.Vars);
-
-    static create(target: Object, vars?: Draggable.Vars): Draggable[];
-    static get(target: Object): Draggable;
-    static hitTest(testObject1: Draggable.TestObject, testObject2: Draggable.TestObject, threshold?: number | string): boolean;
-    static timeSinceDrag(): number;
-
-    addEventListener(type: string, callback: Function);
-    applyBounds(newBounds?: Draggable.Bounds): this;
-    disable(type?: string): this;
-    drag(event: Event): void;
-    enable(type?: string): this;
-    enabled(): boolean;
-    enabled(value: boolean, type?: string): this;
-    endDrag(event: Event): void;
-    getDirection(from: "start", diagonalThreshold?: number): string;
-    getDirection(from: "velocity", diagonalThreshold?: number): string;
-    getDirection(from: Draggable.DOMTarget, diagonalThreshold?: number): string;
-    hitTest(testObject: Draggable.TestObject, threshold?: number | string): boolean;
-    getDirection(from?: string, diagonalThreshold?: number): string;
-    kill(): this;
-    removeEventListener(type: string, callback: Function);
-    startDrag(event: Event): this;
-    timeSinceDrag(): number;
-    update(applyBounds?: boolean): this;
-  }
-
-}
-
 declare namespace Draggable {
 
     type DOMElement = string | Element | List<Element> | JQuery;
